1feea68fd140c423151333244402f0c97fcd4eb2
Kelsie Smith <kelsies@missionreadyhq.com>
2023-09-20 21:48:33 +1200

    Hero complete

--- M5-frontend/src/components/kelsieComponents/homePage/Hero.jsx
+++ M5-frontend/src/components/kelsieComponents/homePage/Hero.jsx
@@ -6,21 +6,21 @@ const Hero = () => {
       <img src={hero} alt="Hero" className="w-full h-full object-cover" />
       <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center text-center">
         <h1
-          className="text-black font-plus-jakarta-sans text-4xl font-extrabold uppercase absolute left-60 w-[574.664px] text-left"
+          className="text-black font-plus-jakarta-sans text-4xl font-bold uppercase absolute left-60 w-[700px] text-left"
           style={{
             lineHeight: "121.2%",
             color: "var(--black, #1A1A1A)",
-            fontSize: "40px",
-            top: "127px",
-            left: "60px",
+            fontSize: "55px",
+            top: "150px",
+            left: "100px",
           }}
         >
           Effortless Property Management, Exceptional Living.
         </h1>
         <div
-          className="mt-55 flex justify-center space-x-4 absolute"
+          className="mt-1 flex justify-center space-x-4 absolute"
           style={{
-            left: "60px",
+            left: "100px",
           }}
         >
           <button
@@ -35,13 +35,13 @@ const Hero = () => {
             }}
           >
             <span
-              className="text-white font-plus-jakarta-sans text-16 font-semibold"
+              className="text-white font-plus-jakarta-sans text-lg  font-semibold"
               style={{ lineHeight: "35px" }}
             >
               Free Appraisal
             </span>
           </button>
-          <span className="text-gray-600 font-plus-jakarta-sans text-16 font-semibold self-center">
+          <span className="text-gray-600 font-plus-jakarta-sans text-lg font-bold self-center px-10">
             Contact us
           </span>
         </div>
